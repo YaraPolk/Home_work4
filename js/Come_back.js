4cc43cf0a9fd75d412693ab1be9945e7c7ecf64a
Yaroslav Ivanitskiy <yaroslav260493@gmail.com>
2018-11-29 21:07:36 +0300

    Первая кнопка

---
+++ js/Come_back.js
@@ -0,0 +1,3 @@
+document.getElementById('start-page').onclick = function() {
+  window.location.href = "index.html";
+}
---
+++ js/Start-page.js
@@ -0,0 +1,3 @@
+document.getElementById('link').onclick = function() {
+    window.location.href = "index1.html";
+}
